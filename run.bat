86a66184cbd6ecfdf6b4ce841778ce227ab0fea8
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-27 17:08:54 +0700

    use my new package

--- run.bat
+++ run.bat
@@ -19,11 +19,11 @@ echo Checking dependencies...
 if exist "..\node_modules" (
     echo Using node_modules from parent directory...
     cd ..
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3
+    CALL npm install user-agents axios meo-forkcy-colors https-proxy-agent socks-proxy-agent ethers web3
     cd %~dp0
 ) else (
     echo Installing dependencies in current directory...
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 
+    CALL npm install user-agents axios meo-forkcy-colors https-proxy-agent socks-proxy-agent ethers web3 
 )
 echo Dependencies installation completed!
 
